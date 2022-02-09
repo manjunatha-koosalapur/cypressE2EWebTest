9ee159b9272577ed92717c60688cd4ff862eda36
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-02-09 17:03:45 +0530

    cmt02

--- cypress/integration/testSuite02/ts01-viewPortTests.js
+++ cypress/integration/testSuite02/ts01-viewPortTests.js
@@ -12,31 +12,31 @@ describe('view ports: Control the size & orientation of the APP screen', functio
         cy.visit('http://www.google.com')
     })
 
-    it('test01: open tests in macbook-16', ()=> {
+    it('test01: run tests in macbook-16', ()=> {
         cy.viewport('macbook-16')
         cy.screenshot()
         cy.wait(2000)
     })
 
-    it('test02: open tests in ipad-2', ()=> {
+    it('test02: run tests in ipad-2', ()=> {
         cy.viewport('ipad-2')
         cy.screenshot()
         cy.wait(2000)
     })
 
-    it('test03: open tests in iphone-xr', ()=> {
+    it('test03: run tests in iphone-xr', ()=> {
         cy.viewport('iphone-xr')
         cy.screenshot()
         cy.wait(2000)
     })
 
-    it('test04: open tests in 1024px x 768px', ()=> {
+    it('test04: run tests in 1024px x 768px', ()=> {
         cy.viewport(1024,768)
         cy.screenshot()
         cy.wait(2000)
     })
 
-    it('test04: open tests in samsung-note9', ()=> {
+    it('test04: run tests in samsung-note9', ()=> {
         cy.viewport('samsung-note9')
         cy.screenshot()
         cy.wait(2000)
